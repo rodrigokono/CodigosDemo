a357b9dc1bc48cc179ccddb39ab507693e12ec48
Rodrigo Kono <rodrigokono@gmail.com>
2012-07-02 00:32:07 -0300

    subindo demo blog

---
+++ TesteParseConvert/TesteParseConvert/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteParseConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConvertTo: " + TempoConvertTo());
+            Console.WriteLine("Parse    : " + TempoParse());
+            Console.WriteLine("TryParse : " + TempoTryParse());
+        }
+
+        static TimeSpan TempoConvertTo()
+        {
+            TimeSpan inicio = DateTime.Now.TimeOfDay;
+            for (int i = 0; i < 90000000; i++)
+            {
+                Convert.ToInt32("01234");
+            }
+            return DateTime.Now.TimeOfDay.Subtract(inicio);
+        }
+
+        static TimeSpan TempoParse()
+        {
+            TimeSpan inicio = DateTime.Now.TimeOfDay;
+            for (int i = 0; i < 90000000; i++)
+            {
+                int.Parse("01234");
+            }
+            return DateTime.Now.TimeOfDay.Subtract(inicio);
+        }
+
+        static TimeSpan TempoTryParse()
+        {
+            int resultado;
+            TimeSpan inicio = DateTime.Now.TimeOfDay;
+            for (int i = 0; i < 90000000; i++)
+            {
+                int.TryParse("01234", out resultado);
+            }
+            return DateTime.Now.TimeOfDay.Subtract(inicio);
+        }
+    }
+}
